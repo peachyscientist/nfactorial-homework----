993bd6adff2d6da2f842b7e963b37eea51c7786a
Anel <40689066+peachyscientist@users.noreply.github.com>
2023-02-17 13:51:50 +0600

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header(){
+    return (
+        <div>
+          <h1>Simple To Do List</h1>  
+          <p>Today is awesome day. The weather is awesome, you are awesome too!</p>
+        </div>
+    )
+}
+
+export default Header;
